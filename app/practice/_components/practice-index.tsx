7150c616fe69249a1c2d5d373e75bb65d4de1635
sam4web <sijal.m06@gmail.com>
2025-10-30 17:45:42 +0545

    relocate `PracticeItemGrid`; fix `useSearchFilter` generics

--- app/practice/_components/practice-index.tsx
+++ app/practice/_components/practice-index.tsx
@@ -1,18 +1,41 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import SearchInput from "@/components/search-input";
 import { DIFFICULTY_MAP, IChallenge, DifficultyKey } from "@/lib/practice";
 import { cx } from "@/lib/utils";
-import PracticeItemGrid from "./practice-item-grid";
+import useSearchFilter from "@/hooks/use-search-filter";
+import { useIsMobile } from "@/hooks/use-mobile";
+import PracticeItem from "./practice-item";
 
 export default function PracticeIndex({ challenges }: { challenges: IChallenge[] }) {
   const [difficulty, setDifficulty] = useState<DifficultyKey | null>(null);
+  const isMobile = useIsMobile();
+  const columns = useMemo(() => {
+    const numColumns = isMobile ? 2 : 3;
+    const columns: IChallenge[][] = Array.from({ length: numColumns }, () => []);
+    challenges.forEach((item, index) => {
+      const columnIndex = index % numColumns;
+      columns[columnIndex].push(item);
+    });
+    return columns;
+  }, [isMobile]);
+
+  const filterChallenges = useCallback((search: string, challenges: IChallenge[]) => {
+    return null;
+  }, []);
+
+  const { search, setSearch, filteredData } = useSearchFilter<IChallenge>(challenges, filterChallenges);
 
   return (
     <>
       <div className="space-y-3 md:space-y-4.5">
-        <SearchInput search={""} onSearchChange={(search) => console.log(search)} placeholder="Search challenges..." />
+        <SearchInput
+          search={search}
+          onSearchChange={(search) => setSearch(search)}
+          placeholder="Search challenges..."
+        />
+        {/* Difficulty Options List */}
         <div className="max-w-sm md:max-w-md mx-auto flex gap-2">
           <p className="font-medium text-sm text-muted-foreground">Difficulty:</p>
           <div className="tag-container">
@@ -29,7 +52,18 @@ export default function PracticeIndex({ challenges }: { challenges: IChallenge[]
         </div>
       </div>
 
-      <PracticeItemGrid challenges={challenges} />
+      {/* Items Grid */}
+      <div className="max-w-sm xs:max-w-xl md:max-w-full mx-auto">
+        <div className="grid xs:grid-cols-2 md:grid-cols-3 gap-3 items-start">
+          {columns.map((col, idx) => (
+            <div key={idx} className="grid gap-y-3">
+              {col.map((item) => (
+                <PracticeItem challenge={item} key={item.id} />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- hooks/use-search-filter.tsx
+++ hooks/use-search-filter.tsx
@@ -3,7 +3,7 @@ import useDebounceEffect from "./use-debounce-effect";
 
 type DataFilter<T, R> = (search: string, cheatsheets: T[]) => R[] | null;
 
-export default function useSearchFilter<T, R>(initialData: T[], dataFilter: DataFilter<T, R>) {
+export default function useSearchFilter<T, R = T>(initialData: T[], dataFilter: DataFilter<T, R>) {
   const [search, setSearch] = useState("");
   const [filteredData, setFilteredData] = useState<R[] | null>(null);
 
